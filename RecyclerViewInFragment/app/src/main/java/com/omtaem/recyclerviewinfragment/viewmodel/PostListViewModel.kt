abc1bb53f2e8eed368db54fbe3fe749aae36e897
OMER FARUK GUZEL <58605364+omerfarukgzl@users.noreply.github.com>
2023-01-03 17:34:29 +0300

    Update PostListViewModel.kt

--- RecyclerViewInFragment/app/src/main/java/com/omtaem/recyclerviewinfragment/viewmodel/PostListViewModel.kt
+++ RecyclerViewInFragment/app/src/main/java/com/omtaem/recyclerviewinfragment/viewmodel/PostListViewModel.kt
@@ -48,29 +48,29 @@ class PostListViewModel :ViewModel(){
     }
     fun refreshData2()
     {
-        val banana = Post("https://www.ufuktarim.com/imaj/blog/sera-muz-yetistiriciligi.jpg",
+        val avocado = Post("https://www.pritikin.com/wp-content/uploads/2022/09/are-avocados-healthy.jpg",
             "https://cdn-icons-png.flaticon.com/512/159/159113.png?w=740&t=st=1672598461~exp=1672599061~hmac=7b10e10a22a7bec7276e6d2a429a554de035f3209e595e93a3e5ebc463a4434b",
-            "This is Banana. Banana is Yellow",
-            "Yenilenen Kullanıcı1")
+            "This is Avocado. Avocado is green",
+            "Ebubekir Guzel")
 
 
-        val strawbarry = Post("https://cdn1.ntv.com.tr/gorsel/J3JsXu1BlkKrNcoJg_pk-A.jpg?width=1000&mode=both&scale=both&v=1652861149667",
+        val charry = Post("https://www.medikalakademi.com.tr/wp-content/uploads/2019/05/kiraz-meyve-14.jpg.webp",
             "https://cdn-icons-png.flaticon.com/512/159/159113.png?w=740&t=st=1672598461~exp=1672599061~hmac=7b10e10a22a7bec7276e6d2a429a554de035f3209e595e93a3e5ebc463a4434b",
-            "This is Strawbarry. Strawbarry is Yellow",
-            "Yenilenen Kullanıcı2")
+            "This is Charry. Charry is Red",
+            "Omer Guzel")
 
-        val orange = Post("https://cdn.yemek.com/mncrop/940/625/uploads/2015/04/portakal.jpg",
+        val mandarin = Post("https://www.acibadem.com.tr/hayat/Images/YayinMakaleler/mandalinanin-kabugunda-bile-saglik-var_6960_1.jpg",
             "https://cdn-icons-png.flaticon.com/512/159/159113.png?w=740&t=st=1672598461~exp=1672599061~hmac=7b10e10a22a7bec7276e6d2a429a554de035f3209e595e93a3e5ebc463a4434b",
-            "This is Orange. Orange is Yellow",
-            "Yenilenen Kullanıcı3")
+            "This is Mandarin. Mandarin is Orange",
+            "Osman Guzel")
 
-        val apple = Post("https://cdnd-tr.ceptesok.com/product/1000x1000/ce378_Elma_Granny_Kg_.jpg",
+        val lemon = Post("https://w0.peakpx.com/wallpaper/969/297/HD-wallpaper-yellow-lemons-with-green-leaves-in-yellow-background-yellow-background.jpg",
             "https://cdn-icons-png.flaticon.com/512/159/159113.png?w=740&t=st=1672598461~exp=1672599061~hmac=7b10e10a22a7bec7276e6d2a429a554de035f3209e595e93a3e5ebc463a4434b",
-            "This is Apple. Apple is Yellow",
-            "Yenilenen Kullanıcı4")
+            "This is Lemon. Lemon is Yellow",
+            "Ali Guzel")
 
         // oluşturulan besinleri array listde saklama - sonradan ekelme çıkarma yapılabilir
-        val postlist= arrayListOf<Post>(banana,strawbarry,apple,orange)
+        val postlist= arrayListOf<Post>(avocado,charry,mandarin,lemon)
 
         // besinler MutableLiveData değişkenine besinlistesini atama
         posts.value=postlist
